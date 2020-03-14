0564c4f1dd81f3fa0427f32999b7208307f57a55
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-03-14 13:50:27 +0100

    Napravljena nova klasa Movie

---
+++ app/src/main/java/ba/unsa/etf/rma/vj_18484/Movie.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rma.vj_18484;
+
+public class Movie {
+    private String title;
+    private String genre;
+    private String releaseDate;
+    private String homepage;
+    private String overview;
+}
